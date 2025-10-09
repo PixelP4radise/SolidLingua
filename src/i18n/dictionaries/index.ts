44c053b7fe3ab83c1541f60c53ac34fb79550e95
madaxen86 <59310516+madaxen86@users.noreply.github.com>
2025-10-09 11:26:34 +0200

    fix naming of pt route to pt-br added fall back to "default" if provided lang does not match a SUPPORTED_LOCALE activated pt-br dictionaries

--- src/i18n/dictionaries/index.ts
+++ src/i18n/dictionaries/index.ts
@@ -1,12 +1,12 @@
 import english from "./en/ui";
-// import ptbr from "./pt-br/ui";
+import ptbr from "./pt-br/ui";
 
 export const dictionaries = {
-	default: english,
-	// "pt-br": ptbr,
+  default: english,
+  "pt-br": ptbr,
 };
 
 export const languages: { [key: string]: string } = {
-	en: "English",
-	// "pt-br": "Português do Brasil",
+  en: "English",
+  "pt-br": "Português do Brasil",
 };
--- src/i18n/i18n-context.tsx
+++ src/i18n/i18n-context.tsx
@@ -1,4 +1,4 @@
-import { useMatch } from "@solidjs/router";
+import { useMatch, useParams } from "@solidjs/router";
 import { type JSX, createContext, useContext } from "solid-js";
 import { createStore } from "solid-js/store";
 import { createTranslator } from "./translator";
@@ -7,29 +7,25 @@ import { SUPPORTED_LOCALES } from "./config";
 type ProviderProps = { children: JSX.Element };
 
 const initialI18nStore = {
-	get t() {
-		const match = useMatch(() => "/:locale?/*", {
-			locale: SUPPORTED_LOCALES,
-		});
-
-		return createTranslator(match()?.params.project ?? null);
-	},
+  get t() {
+    const param = useParams<{ lang?: string }>()?.lang ?? "default";
+    const lang = SUPPORTED_LOCALES.includes(param) ? param : "default";
+    return createTranslator(lang);
+  },
 };
 
 export const I18nContext = createContext(initialI18nStore);
 
 export function I18nProvider(props: ProviderProps) {
-	const [i18n] = createStore(initialI18nStore);
+  const [i18n] = createStore(initialI18nStore);
 
-	return (
-		<I18nContext.Provider value={i18n}>{props.children}</I18nContext.Provider>
-	);
+  return <I18nContext.Provider value={i18n}>{props.children}</I18nContext.Provider>;
 }
 
 export function useI18n() {
-	const context = useContext(I18nContext);
-	if (!context) {
-		throw new Error("useI18n: must be used inside <I18nProvider />");
-	}
-	return context;
+  const context = useContext(I18nContext);
+  if (!context) {
+    throw new Error("useI18n: must be used inside <I18nProvider />");
+  }
+  return context;
 }
