64ecec1af3f0cb7ff8d3b2a90c7693dc572ca361
madaxen86 <59310516+madaxen86@users.noreply.github.com>
2025-10-09 11:06:30 +0200

    added i18nProvider to app.tsx fix: routes need "export default" to be added as file route.

--- src/routes/[lang]/index.tsx
+++ src/routes/[lang]/index.tsx
@@ -1,13 +1,11 @@
-import {Show} from "solid-js";
-import {useI18n} from "~/i18n/i18n-context";
+import { Show } from "solid-js";
+import { useI18n } from "~/i18n/i18n-context";
 
-export function LandingPage() {
-    const i18n = useI18n();
-    return (
-        <Show when={i18n.t}>
-            <div>
-                {i18n.t("pagination.previous")}
-            </div>
-        </Show>
-    );
+export default function LandingPage() {
+  const i18n = useI18n();
+  return (
+    <Show when={i18n.t}>
+      <div>{i18n.t("pagination.previous")}</div>
+    </Show>
+  );
 }
